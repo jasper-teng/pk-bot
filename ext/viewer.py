148bf2f0945e8201d2a4aad3ef3a888c4bd968c0
silverhawke249 <hi@silverhawke.xyz>
2021-06-30 02:14:06 +0800

    implement queueing and reply feature on requests

--- ext/viewer.py
+++ ext/viewer.py
@@ -1,3 +1,4 @@
+import asyncio
 import os
 import subprocess
 import time
@@ -7,7 +8,14 @@ from discord.ext import commands
 from dotenv import load_dotenv
 from ext._scraper import update_score, update_songs
 
-IS_PROCESSING = False
+# Global state
+IS_PROCESSING = 0
+SCORE_QUEUED = asyncio.Event()
+SCORE_QUEUED.set()
+
+# Script constant
+PROCESS_SONG = 1
+PROCESS_SCORE = 2
 
 load_dotenv()
 ROLE_ID = int(os.getenv('BOT_HANDLER_ID'))
@@ -27,14 +35,22 @@ async def viewer(ctx):
 
 @viewer.command()
 async def scoreupdate(ctx, *sdvx_ids):
-    """ Updates the scores in the viewer """
-    global IS_PROCESSING
+    """
+    Updates the scores in the viewer.
+    
+    Send command with no arguments to update all scores in the database.
+    """
+    global IS_PROCESSING, SCORE_QUEUED
 
-    if IS_PROCESSING:
+    if IS_PROCESSING == PROCESS_SCORE:
+        await ctx.reply('This request is now queued.')
+        await SCORE_QUEUED.wait()
+    elif IS_PROCESSING == PROCESS_SONG:
         await ctx.message.add_reaction('⛔')
         await ctx.send('Please wait until the currently running process finishes.', delete_after=10)
         return
-    IS_PROCESSING = True
+    IS_PROCESSING = PROCESS_SCORE
+    SCORE_QUEUED.clear()
 
     import time
     cur_time = time.localtime()
@@ -46,31 +62,33 @@ async def scoreupdate(ctx, *sdvx_ids):
     try:
         await update_score(message, EVENT_LOOP, sdvx_ids)
     except Exception as e:
-        IS_PROCESSING = False
+        IS_PROCESSING = 0
+        SCORE_QUEUED.set()
         raise e
 
     subprocess.call('git add scores/.', stdout=DEVNULL, cwd=VIEWER_DIR)
     subprocess.call(f'git commit scores/. -m "automated score update ({time.strftime("%Y%m%d%H%M%S", cur_time)})"', stdout=DEVNULL, cwd=VIEWER_DIR)
-    subprocess.call('git push --porcelain', stdout=DEVNULL, cwd=VIEWER_DIR)
+    subprocess.call('git push --porcelain', stdout=DEVNULL, stderr=DEVNULL, cwd=VIEWER_DIR)
 
     embed = Embed(title='SDVX score scraper', description='Automated score update finished.')
-    await ctx.send(embed=embed)
+    await ctx.reply(embed=embed)
     await message.delete(delay=10)
 
-    IS_PROCESSING = False
+    IS_PROCESSING = 0
+    SCORE_QUEUED.set()
 
 
 @viewer.command()
 @commands.has_role(ROLE_ID)
 async def songupdate(ctx, is_full_update=False):
-    """ Updates the song database in the viewer """
+    """ Updates the song database in the viewer. """
     global IS_PROCESSING
 
     if IS_PROCESSING:
         await ctx.message.add_reaction('⛔')
         await ctx.send('Please wait until the currently running process finishes.', delete_after=10)
         return
-    IS_PROCESSING = True
+    IS_PROCESSING = PROCESS_SONG
 
     import time
     cur_time = time.localtime()
@@ -82,11 +100,11 @@ async def songupdate(ctx, is_full_update=False):
     try:
         new_songs = await update_songs(EVENT_LOOP, is_full_update)
     except Exception as e:
-        IS_PROCESSING = False
+        IS_PROCESSING = 0
         raise e
 
     subprocess.call(f'git commit song_db.json -m "automated song db update ({time.strftime("%Y%m%d%H%M%S", cur_time)})"', stdout=DEVNULL, cwd=VIEWER_DIR)
-    subprocess.call('git push --porcelain', stdout=DEVNULL, cwd=VIEWER_DIR)
+    subprocess.call('git push --porcelain', stdout=DEVNULL, stderrt=DEVNULL, cwd=VIEWER_DIR)
 
     if new_songs:
         desc = ['Automated song database update finished. Added the following songs:']
@@ -98,7 +116,7 @@ async def songupdate(ctx, is_full_update=False):
     embed = Embed(title='SDVX score scraper', description='\n'.join(desc))
     await message.edit(embed=embed)
 
-    IS_PROCESSING = False
+    IS_PROCESSING = 0
 
 
 def setup(bot):
