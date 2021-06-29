6a48220f585645c85dfb660d31399460ffb97031
silverhawke249 <hi@silverhawke.xyz>
2021-06-30 01:52:43 +0800

    add "sweetland" command

--- ext/miscellaneous.py
+++ ext/miscellaneous.py
@@ -7,5 +7,11 @@ async def notthemxm(ctx):
     await ctx.send(file=File('ext/notthemxm.png'))
 
 
+@commands.command()
+async def sweetland(ctx):
+    await ctx.send('https://www.youtube.com/watch?v=ws4vY996y8M')
+
+
 def setup(bot):
     bot.add_command(notthemxm)
+    bot.add_command(sweetland)
