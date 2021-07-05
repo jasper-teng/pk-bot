3dccac4ed312e92554c8d487fcc97830d9960ea4
silverhawke249 <hi@silverhawke.xyz>
2021-07-05 17:18:22 +0800

    improve `sweetland play` behavior, add `ligma` command

--- ext/miscellaneous.py
+++ ext/miscellaneous.py
@@ -1,6 +1,6 @@
 import asyncio
 import random
-from discord import File, FFmpegOpusAudio
+from discord import File, FFmpegOpusAudio, DeletedReferencedMessage
 from discord.ext import commands
 
 VOICE_CLIENT = None
@@ -22,6 +22,16 @@ async def bread(ctx):
         await ctx.send('bread craftsingle banana craftsingle')
 
 
+@commands.command()
+async def ligma(ctx):
+    """ Who the hell is Steve Jobs?! """
+    msg = ctx.message
+    if msg.reference is None or isinstance(msg.reference.resolved, DeletedReferencedMessage):
+        await ctx.send(file=File('ext/ligma.jpg'))
+    else:
+        await msg.reference.resolved.reply(file=File('ext/ligma.jpg'))
+
+
 @commands.group(invoke_without_command=True, aliases=['sweetsland'])
 async def sweetland(ctx):
     """ ♫♫♩～ """
@@ -30,10 +40,34 @@ async def sweetland(ctx):
 
 
 @sweetland.command()
-async def play(ctx):
+async def play(ctx, channel_num=None):
+    """
+    Play music in a voice channel.
+    
+    If channel number isn't given, defaults to the user's voice channel.
+    If user isn't in a voice channel, defaults to the first voice channel that is occupied.
+    """
     global VOICE_CLIENT
     if VOICE_CLIENT is not None:
+        ctx.reply('Stop current playback first before starting it again!', delete_after=10)
         return
+
+    # Select channel
+    target_channel = None
+    if channel_num is None:
+        author = ctx.author
+        if author.voice and author.voice.channel is not None:
+            target_channel = author.voice.channel
+        else:
+            for channel in ctx.guild.voice_channels:
+                if channel.members:
+                    target_channel = channel
+                    break
+            await ctx.reply('No voice channel available to bind to!')
+    else:
+        target_channel = ctx.guild.voice_channels[int(channel_num) - 1]
+    audio = FFmpegOpusAudio('ext/sweetland.mp3')
+    await ctx.message.add_reaction('🆗')
 
     stop_event = asyncio.Event()
     loop = asyncio.get_event_loop()
@@ -44,11 +78,7 @@ async def play(ctx):
             stop_event.set()
         loop.call_soon_threadsafe(clear)
 
-    channel = ctx.guild.voice_channels[0]
-    audio = FFmpegOpusAudio('ext/sweetland.mp3')
-    await ctx.message.add_reaction('🆗')
-
-    client = await channel.connect()
+    client = await target_channel.connect()
     VOICE_CLIENT = client
     client.play(audio, after=after)
 
@@ -74,4 +104,5 @@ async def stop(ctx):
 def setup(bot):
     bot.add_command(notthemxm)
     bot.add_command(bread)
+    bot.add_command(ligma)
     bot.add_command(sweetland)
