c0d7762a65ae5ecc530a1a7e9c843f12f39ae259
silverhawke249 <hi@silverhawke.xyz>
2021-12-31 13:42:18 +0700

    change error handling to log into file

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 # bot.py
 import discord
+import json
 import os
+import time
 import traceback
 
 from discord.ext import commands
@@ -9,6 +11,7 @@ from dotenv import load_dotenv
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 ROLE_ID = int(os.getenv('BOT_HANDLER_ID'))  # should be 839485641339306044
+TRACEBACK_LOG_PATH = 'errlog.json'
 
 intents = discord.Intents.default()
 intents.members = True
@@ -47,9 +50,23 @@ async def reload(ctx, *args):
 @bot.listen('on_command_error')
 async def error_handler(ctx, err):
     if not isinstance(err, commands.CommandNotFound):
-        tb = ''.join(traceback.format_exception(type(err), err, err.__traceback__, limit=2))
+        tb = ''.join(traceback.format_exception(type(err), err, err.__traceback__, limit=5))
         await ctx.message.add_reaction('⛔')
-        await ctx.reply(f'{type(err).__name__}: {err}\nTraceback: ```{tb}```', delete_after=10)
+        try:
+            with open(TRACEBACK_LOG_PATH, 'r') as f:
+                traceback_log = json.load(f)
+        except IOError:
+            traceback_log = []
+        data = {
+            'time': time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()),
+            'author': f'{ctx.author.name}#{ctx.author.discriminator}',
+            'msg_link': ctx.message.jump_url,
+            'traceback': f'{type(err).__name__}: {err}\n{tb}'
+        }
+        traceback_log.append(data)
+        with open(TRACEBACK_LOG_PATH, 'r') as f:
+            json.dump(traceback_log, f)
+        # await ctx.reply(f'{type(err).__name__}: {err}\nTraceback: ```{tb}```', delete_after=10)
 
 
 @bot.event
